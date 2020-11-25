ff9289011ea82a8277c0c5101ca6f744d28b9160
Mohamed-Omar <mero2online@gmail.com>
2020-11-25 04:26:19 +0200

    Add URL validity checker and validity Alert

--- src/client/js/urlChecker.js
+++ src/client/js/urlChecker.js
@@ -1,16 +1,32 @@
 function checkForURL(inputURL) {
   console.log('::: Running checkForURL :::', inputURL);
 
+  let validityAlert = document.querySelector('.validityAlert');
+  let validityAlert_message = document.querySelector('.validityAlert-message');
+
   function validURL(inputURL) {
-    var regexp = /(?:http[s]?\/\/)?(?:[\w\-]+(?::[\w\-]+)?@)?(?:[\w\-]+\.)+(?:[a-z]{2,4})(?::[0-9]+)?(?:\/[\w\-\.%]+)*(?:\?(?:[\w\-\.%]+=[\w\-\.%!]+&?)+)?(#\w+\-\.%!)?/;
+    var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
     return regexp.test(inputURL);
   }
 
+  if (
+    typeof validityAlert_message != 'undefined' &&
+    validityAlert_message != null
+  ) {
+    validityAlert_message.remove();
+  }
+
   if (validURL(inputURL)) {
-    alert('Url is valid');
+    validityAlert.insertAdjacentHTML(
+      'afterbegin',
+      '<div class="validityAlert-message">URL is valid</div>'
+    );
     return true;
   }
-  alert('Url is not valid!');
+  validityAlert.insertAdjacentHTML(
+    'afterbegin',
+    '<div class="validityAlert-message">URL is not valid!</div>'
+  );
   return false;
 }
 
