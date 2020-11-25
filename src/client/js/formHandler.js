646e8ee4c44145d7a76a7164cc798eab114d16ca
Mohamed-Omar <mero2online@gmail.com>
2020-11-25 03:44:20 +0200

    Adjust inputURL

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,25 +2,25 @@ function handleSubmit(event) {
   event.preventDefault();
 
   // check what text was put into the form field
-  let formText = document.getElementById('name').value;
-  Client.checkForName(formText);
+  let inputURL = document.getElementById('url-input').value;
+  Client.checkForURL(inputURL);
 
   fetch('/api')
     .then((data) => data.json())
     .then(function (data) {
       let apiKey = data.apiKey;
       // Base URL for MeaningCloud API
-      let baseURL = `https://api.meaningcloud.com/sentiment-2.1?key=${apiKey}&url=${formText}&lang=en`;
+      let baseURL = `https://api.meaningcloud.com/sentiment-2.1?key=${apiKey}&url=${inputURL}&lang=en`;
       console.log('::: Form Submitted :::');
       fetch(baseURL)
         .then((res) => res.json())
         .then(function (res) {
           console.log(res);
-          document.getElementById('agreement-result').innerHTML = `Agreement: ${res.agreement.toLowerCase()}`
+          document.getElementById('agreement-result').innerHTML = `Agreement: ${res.agreement}`
           document.getElementById('confidence-result').innerHTML = `Confidence: ${res.confidence}`
-          document.getElementById('irony-result').innerHTML = `Irony: ${res.irony.toLowerCase()}`
+          document.getElementById('irony-result').innerHTML = `Irony: ${res.irony}`
           document.getElementById('model-result').innerHTML = `Model: ${res.model}`
-          document.getElementById('subjectivity-result').innerHTML = `Subjectivity: ${res.subjectivity.toLowerCase()}`;
+          document.getElementById('subjectivity-result').innerHTML = `Subjectivity: ${res.subjectivity}`;
           document.getElementById('score-tag-result').innerHTML = `Score Tag: ${res.score_tag}`;
         });
     });
