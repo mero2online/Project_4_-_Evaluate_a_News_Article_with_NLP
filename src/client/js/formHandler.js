3c5dfb29761290fcf32609fb5d1b5e7c57a85fb1
Mohamed-Omar <mero2online@gmail.com>
2020-12-23 08:41:46 +0200

    Add key

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -32,7 +32,7 @@ function handleSubmit(event) {
   // fetch('/api')
   //   .then((data) => data.json())
   //   .then(function (data) {
-      let apiKey = process.env.API_KEY;
+      let apiKey = '33cbdafdbb6906cb9f0a61b75cbaa159';
 
       // Base URL for MeaningCloud API
       let baseURL = `https://api.meaningcloud.com/sentiment-2.1?key=${apiKey}&url=${inputURL}&lang=en`;
